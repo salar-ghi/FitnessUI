d70ea0c32ebab0d5e133dd312b1ebc110fb8bdab
salar-ghi <salar.ghi1993@gmail.com>
2024-03-24 11:19:08 +0330

    adding some config to repo for wpf and color

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url)
+
+// import runtimeCaching from "next-pwa/cache"
+// import Caching from "next-pwa/cache"
+import PWA from "next-pwa"
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const runtimeCaching = require("next-pwa/cache");
+// const runtimeCaching = Caching;
+const withPWA = PWA({
+    dest : 'public',
+    register: true,
+    skipWaiting: true,
+    // cacheOnFrontEndNav: true,
+    // aggressiveFrontEndNavCaching: true,
+    // reloadOnOnline: true,
+    // swcMinify:true,
+    // runtimeCaching
+})
+
+const nextConfig = withPWA({
+    reactStrictMode : false,
+    // swcMinify:false
+});
 
 export default nextConfig;
